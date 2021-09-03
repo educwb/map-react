92ca2a8424d1b84825964a8e3abca73a675b6143
Eduardo Silva <educwb@gmail.com>
2021-09-02 21:21:11 -0300

    correcao stackoverflow.com questions 67552020

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { MapContainer, TileLayer } from 'react-leaflet'
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={[0, 0]} zoom={4}>
+      <TileLayer
+        attribution="&amp;copy <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+        url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+      />
+        {/* <TileLayer 
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+          attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" /> */}
+      </MapContainer>
     </div>
   );
 }
